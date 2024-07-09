c8a7813eca4fdc029b8651eefc2dc7cb19a64c44
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-07-09 22:21:23 +0330

    update => fix the ref in Form project

--- components/template/form/project/Form.tsx
+++ components/template/form/project/Form.tsx
@@ -15,128 +15,127 @@ import SubmitLoadingButton from "@/components/modules/SubmitLoadingButton"
 import { TypeProjectInput } from "@/lib/types"
 
 type FormProps = {
-  ref: React.RefObject<HTMLFormElement>
   defaultValues?: TypeProjectInput | null
   submitText: string
   errors: TypeError
   submitFunction: (formData: FormData) => void | any
 }
+const Form = React.forwardRef<HTMLFormElement, FormProps>(
+  ({ defaultValues, submitText, submitFunction, errors }, ref) => {
+    return (
+      <form ref={ref} action={submitFunction} className="[&>section]:mt-6 [&>section>*]:mb-3">
+        <section>
+          <Typography variant="subtitle1" component={"h5"}>
+            Image
+          </Typography>
+          <TextError message={errors && errors.image}>
+            <input
+              name="image"
+              type="file"
+              className={`w-full p-4 rounded-md border border-solid ${
+                errors && errors?.image ? "border-red-500" : "border-white"
+              }`}
+            />
+            {defaultValues && defaultValues.image && <span>Default : {defaultValues.image}</span>}
+          </TextError>
+        </section>
 
-const Form: React.FC<FormProps> = ({ ref, defaultValues, submitText, errors, submitFunction }) => {
-  return (
-    <form ref={ref} action={submitFunction} className="[&>section]:mt-6 [&>section>*]:mb-3">
-      <section>
-        <Typography variant="subtitle1" component={"h5"}>
-          Image
-        </Typography>
-        <TextError message={errors && errors.image}>
-          <input
-            name="image"
-            type="file"
-            className={`w-full p-4 rounded-md border border-solid ${
-              errors && errors?.image ? "border-red-500" : "border-white"
-            }`}
-          />
-          {defaultValues && defaultValues.image && <span>Default : {defaultValues.image}</span>}
-        </TextError>
-      </section>
+        <section>
+          <Typography variant="subtitle1" component={"h5"}>
+            Title
+          </Typography>
+          <TextError message={errors && errors.title}>
+            <TextField
+              size="small"
+              className="w-full"
+              name="title"
+              placeholder="like next js dashboard"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <TitleIcon />
+                  </InputAdornment>
+                ),
+              }}
+              variant="outlined"
+              error={Boolean(errors && errors?.title)}
+              defaultValue={defaultValues && defaultValues.title}
+            />
+          </TextError>
+        </section>
 
-      <section>
-        <Typography variant="subtitle1" component={"h5"}>
-          Title
-        </Typography>
-        <TextError message={errors && errors.title}>
-          <TextField
-            size="small"
-            className="w-full"
-            name="title"
-            placeholder="like next js dashboard"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <TitleIcon />
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-            error={Boolean(errors && errors?.title)}
-            defaultValue={defaultValues && defaultValues.title}
-          />
-        </TextError>
-      </section>
+        <section>
+          <Typography variant="subtitle1" component={"h5"}>
+            Project Link
+          </Typography>
+          <TextError message={errors && errors.link}>
+            <TextField
+              size="small"
+              className="w-full"
+              name="link"
+              placeholder="https://project.com"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <InsertLinkIcon />
+                  </InputAdornment>
+                ),
+              }}
+              variant="outlined"
+              error={Boolean(errors && errors?.link)}
+              defaultValue={defaultValues && defaultValues.link}
+            />
+          </TextError>
+        </section>
 
-      <section>
-        <Typography variant="subtitle1" component={"h5"}>
-          Project Link
-        </Typography>
-        <TextError message={errors && errors.link}>
-          <TextField
-            size="small"
-            className="w-full"
-            name="link"
-            placeholder="https://project.com"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <InsertLinkIcon />
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-            error={Boolean(errors && errors?.link)}
-            defaultValue={defaultValues && defaultValues.link}
-          />
-        </TextError>
-      </section>
+        <section>
+          <Typography variant="subtitle1" component={"h5"}>
+            Source Link
+          </Typography>
+          <TextError message={errors && errors.source}>
+            <TextField
+              size="small"
+              className="w-full"
+              name="source"
+              placeholder="https://source.com"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <InsertLinkIcon />
+                  </InputAdornment>
+                ),
+              }}
+              variant="outlined"
+              error={Boolean(errors && errors?.source)}
+              defaultValue={defaultValues && defaultValues.source}
+            />
+          </TextError>
+        </section>
 
-      <section>
-        <Typography variant="subtitle1" component={"h5"}>
-          Source Link
-        </Typography>
-        <TextError message={errors && errors.source}>
-          <TextField
-            size="small"
-            className="w-full"
-            name="source"
-            placeholder="https://source.com"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <InsertLinkIcon />
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-            error={Boolean(errors && errors?.source)}
-            defaultValue={defaultValues && defaultValues.source}
-          />
-        </TextError>
-      </section>
+        <section>
+          <Typography variant="subtitle1" component={"h5"}>
+            Desc
+          </Typography>
+          <TextError message={errors && errors.description}>
+            <TextField
+              size="small"
+              className="w-full"
+              name="description"
+              placeholder="about project"
+              multiline
+              rows={4}
+              variant="outlined"
+              error={Boolean(errors && errors?.description)}
+              defaultValue={defaultValues && defaultValues.description}
+            />
+          </TextError>
+        </section>
 
-      <section>
-        <Typography variant="subtitle1" component={"h5"}>
-          Desc
-        </Typography>
-        <TextError message={errors && errors.description}>
-          <TextField
-            size="small"
-            className="w-full"
-            name="description"
-            placeholder="about project"
-            multiline
-            rows={4}
-            variant="outlined"
-            error={Boolean(errors && errors?.description)}
-            defaultValue={defaultValues && defaultValues.description}
-          />
-        </TextError>
-      </section>
-
-      <section>
-        <SubmitLoadingButton submitText={submitText} variant="text" />
-      </section>
-    </form>
-  )
-}
-
+        <section>
+          <SubmitLoadingButton submitText={submitText} variant="contained" />
+        </section>
+      </form>
+    )
+  }
+)
 export default Form
