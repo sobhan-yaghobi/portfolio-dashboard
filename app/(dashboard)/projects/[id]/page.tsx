38042fb8a7c2a62b12744f95137a04b5b2eba1c8
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-07-09 22:01:42 +0330

    update => edit project page

--- app/(dashboard)/projects/[id]/page.tsx
+++ app/(dashboard)/projects/[id]/page.tsx
@@ -1,14 +1,21 @@
 import React from "react"
+import prisma from "@/lib/prisma"
+import { redirect } from "next/navigation"
+import { ProjectCreateInput } from "@/lib/types"
+
 import Typography from "@mui/material/Typography"
 import EditProject from "@/components/template/form/project/EditProject"
-import prisma from "@/lib/prisma"
-import { ProjectCreateInput } from "@/lib/types"
 
 const Page: React.FC<{
   params: { [slug: string]: string }
 }> = async ({ params }) => {
   const id = params?.id
   const mainProject = await prisma.project.findUnique({ where: { id }, select: ProjectCreateInput })
+
+  if (!mainProject) {
+    redirect("/projects")
+  }
+
   return (
     <>
       <Typography variant="h4" component="h2" className="mb-8">
