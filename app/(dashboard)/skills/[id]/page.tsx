968ddd3ef753a946f426d2b165ccbf51c7b5ed20
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-07-11 15:52:40 +0330

    update => edit skill page

--- app/(dashboard)/skills/[id]/page.tsx
+++ app/(dashboard)/skills/[id]/page.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import prisma from "@/lib/prisma"
 import { redirect } from "next/navigation"
-import { ProjectCreateInput, SkillCreateInput } from "@/lib/types"
 
 import Typography from "@mui/material/Typography"
 import EditSkill from "@/components/template/form/skills/EditSkill"
@@ -10,9 +9,9 @@ const Page: React.FC<{
   params: { [slug: string]: string }
 }> = async ({ params }) => {
   const id = params?.id
-  const mainSkills = await prisma.skills.findUnique({ where: { id }, select: SkillCreateInput })
+  const mainSkill = await prisma.skills.findUnique({ where: { id } })
 
-  if (!mainSkills) {
+  if (!mainSkill) {
     redirect("/skills")
   }
 
@@ -23,7 +22,7 @@ const Page: React.FC<{
       <Typography variant="h4" component="h2" className="mb-8">
         Edit Project
       </Typography>
-      <EditSkill id={id} defaultValues={mainSkills} projects={projects} />
+      <EditSkill id={id} defaultValues={mainSkill} projects={projects} />
     </>
   )
 }
