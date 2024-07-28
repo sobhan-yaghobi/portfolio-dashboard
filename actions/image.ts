174f00485e97ad4f8768c012a576bdfef3ce658c
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-07-28 19:21:40 +0330

    update => image and connect database project to supabase

--- actions/image.ts
+++ actions/image.ts
@@ -5,14 +5,12 @@ import supabase from "@/lib/supabase"
 import { env } from "process"
 
 const bucket = env.NEXT_PUBLIC_IMAGE_BUCKET
-const token = env.NEXT_PUBLIC_SUPABASE_SECRET_KEY
 
-type uploadImageWithSignedUrl = {
+type TypeUploadImageParam = {
   image: {
     file: File
     path: string
   }
-  token: string
   bucket: string
 }
 
@@ -20,36 +18,18 @@ export const createImage = async (
   imagePath: string,
   file: File
 ): Promise<TypeReturnSererAction> => {
-  if (!bucket || !token) return { message: "Token or bucket name not found", status: false }
+  if (!bucket) return { message: "bucket name not found", status: false }
 
-  const createSignedUrlResult = await createSignedUploadUrl(imagePath, bucket)
-
-  if (createSignedUrlResult.signedData) {
-    const { path, token } = createSignedUrlResult.signedData
-    return uploadImageWithSignedUrl({ image: { file, path }, bucket, token })
-  }
-
-  return { message: "Create path for image failure", status: false }
+  return await uploadImage({ image: { file, path: imagePath }, bucket })
 }
 
-const createSignedUploadUrl = async (imagePath: string, bucket: string) => {
-  const { data: signedData, error } = await supabase.storage
-    .from(bucket)
-    .createSignedUploadUrl(imagePath)
-
-  if (signedData) return { signedData }
-
-  return { error }
-}
-
-const uploadImageWithSignedUrl = async ({
+const uploadImage = async ({
   image,
-  token,
   bucket,
-}: uploadImageWithSignedUrl): Promise<TypeReturnSererAction> => {
+}: TypeUploadImageParam): Promise<TypeReturnSererAction> => {
   const { data: imageUploadResult } = await supabase.storage
     .from(bucket)
-    .uploadToSignedUrl(image.path, token, image.file)
+    .upload(image.path, image.file)
 
   if (imageUploadResult)
     return { message: "Image successfully created", status: true, data: imageUploadResult.path }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,19 @@
-import { hashPassword } from "../auth/auth"
 import { PrismaClient } from "@prisma/client"
-import { env } from "process"
+
 const prisma = new PrismaClient()
 
 const main = async () => {
   await prisma.admin.deleteMany()
-  await prisma.admin.create({
-    data: {
-      password: await hashPassword(env.password as string),
-      image: "",
-      name: "سبحان یعقوبی",
-      phone: "989396007232",
-      email: "sobhan.yaghobi.work@gmail.com",
-      location: "ایران ، مشهد",
-      bio: "سلام من سبحان یعقوبی یک برنامه نویس فرانت اند با عشق به دنیا هستمکدنویسی، طراحی و وب من عاشق چیزهای جدید و ایجاد پروژه های جالب هستم.وقتی صحبت از کدنویسی و پروژه های فنی می شود. من همیشه زمانی برای لذت بردن از زندگی پیدا می کنم وسرگرمی هایم مانند ورزش و مطالعه را دنبال کنید (من باید مطالعه خود را افزایش دهمزمان). ترکیب زندگی شخصی با حرفه ای که به آن علاقه دارم بهترین استراه رسیدن به موفقیت و رضایت شغلی من همیشه سعی کردم بین اینها تعادل برقرار کنمدو و از هر دو لذت ببرید",
-    },
-  })
+  await prisma.project.deleteMany()
+  await prisma.skills.deleteMany()
+  await prisma.technicalGrowth.deleteMany()
 }
 main()
   .then(async () => {
     await prisma.$disconnect()
   })
   .catch(async (e) => {
-    console.error(e)
+    console.error("error : ", e)
     await prisma.$disconnect()
     process.exit(1)
   })
