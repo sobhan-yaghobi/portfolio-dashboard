9a1c4780c96162c09cb896c40de8d566a2c08e76
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-07-29 13:45:26 +0330

    remove => the old technicalGrowth file action and comment the api route of technicalGrowth

--- app/api/tec_growth/route.tsx
+++ app/api/tec_growth/route.tsx
@@ -1,16 +1,15 @@
-import { get } from "@/actions/TechnicalGrowth"
 import { NextRequest } from "next/server"
 
 export const dynamic = "force-dynamic"
 
-export const GET = async () => {
-  const data = await get()
-  return Response.json(data, {
-    status: 201,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-  })
-}
+// export const GET = async () => {
+//   const data = await get()
+//   return Response.json(data, {
+//     status: 201,
+//     headers: {
+//       "Access-Control-Allow-Origin": "*",
+//       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+//       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+//     },
+//   })
+// }
