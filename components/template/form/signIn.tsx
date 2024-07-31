f390f67d7035bd72915f78fdcf75d87ae8c8140d
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-07-31 15:24:18 +0330

    clean => signIn form component

--- components/template/form/signIn.tsx
+++ components/template/form/signIn.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import React, { useRef, useState } from "react"
-import { toast } from "react-toastify"
+import { showActionReturnMessage } from "@/lib/utils"
+
+import { TypeError } from "@/lib/definition"
 
 import { signInFormAction } from "@/actions/signIn"
-
-import { TypeError, TypeReturnSererAction } from "@/lib/definition"
 
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail"
 import KeyIcon from "@mui/icons-material/Key"
@@ -20,37 +20,28 @@ const SignIn: React.FC = () => {
   const formRef = useRef<HTMLFormElement>(null)
   const [errors, setErrors] = useState<TypeError>({} as TypeError)
 
-  const action = async (event: FormData) => {
+  const clientAction = async (event: FormData) => {
     const actionResult = await signInFormAction(event)
 
     if ("errors" in actionResult) return setErrors({ ...actionResult.errors } as TypeError)
 
-    showMessage(actionResult)
+    showActionReturnMessage({ actionResult, functions: { doActionIfTrue: resetForm } })
     resetForm()
   }
 
-  const showMessage = (actionResult: TypeReturnSererAction) => {
-    const { message } = actionResult
-    if (actionResult.status) {
-      message && toast.success(message)
-    } else {
-      message && toast.error(message)
-    }
-  }
-
   const resetForm = () => {
-    setErrors({} as TypeError)
     formRef.current?.reset()
+    setErrors({} as TypeError)
   }
 
   return (
     <form
       className="bg-black/20 max-w-96 p-6 backdrop-blur-lg shadow-xl shadow-black/10 rounded-lg absolute [&>*]:mt-1 [&]:first:*:!mt-0"
       ref={formRef}
-      action={action}
+      action={clientAction}
     >
       <section className="mb-6">
-        <Typography variant="h4" component="h2" className="mt-0">
+        <Typography component="h2" className="mt-0" variant="h4">
           ورود
         </Typography>
         <p>خوش برگشتید!</p>
@@ -62,9 +53,10 @@ const SignIn: React.FC = () => {
             autoComplete="off"
             dir="ltr"
             error={Boolean(errors && errors?.email)}
-            size="small"
-            name="email"
             placeholder="email"
+            name="email"
+            size="small"
+            variant="outlined"
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -72,7 +64,6 @@ const SignIn: React.FC = () => {
                 </InputAdornment>
               ),
             }}
-            variant="outlined"
           />
         </TextError>
       </section>
@@ -83,10 +74,11 @@ const SignIn: React.FC = () => {
             autoComplete="off"
             dir="ltr"
             error={Boolean(errors && errors?.password)}
+            placeholder="password"
+            name="password"
             size="small"
             type="password"
-            name="password"
-            placeholder="password"
+            variant="outlined"
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -94,12 +86,11 @@ const SignIn: React.FC = () => {
                 </InputAdornment>
               ),
             }}
-            variant="outlined"
           />
         </TextError>
       </section>
 
-      <section className="w-full">
+      <section>
         <SubmitLoadingButton submitText="ادامه" variant="text" />
       </section>
     </form>
