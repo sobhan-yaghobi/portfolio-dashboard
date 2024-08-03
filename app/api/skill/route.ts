aee318864d27460a8a4a60b42f7842b3418dac25
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-08-03 16:44:42 +0330

    build => skill api route

---
+++ app/api/skill/route.ts
@@ -0,0 +1,16 @@
+import prisma from "@/lib/prisma"
+import { NextRequest } from "next/server"
+
+export const dynamic = "force-dynamic"
+
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = request.nextUrl
+  const params = new URLSearchParams(searchParams)
+  const isProjectListInclude = params.get("ProjectListInclude")
+
+  const skillList = await prisma.skill.findMany({
+    include: { projectList: !!isProjectListInclude },
+  })
+
+  return Response.json(skillList || [], { status: 201 })
+}
