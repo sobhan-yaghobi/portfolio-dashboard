3910840eaaa7fa67824fb6d1526b97fce2ca4b53
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-08-12 11:25:44 +0330

    fix => bug in cv route

--- app/api/cv/route.ts
+++ app/api/cv/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server"
 
 export const GET = async () => {
   const technicalSkillList = await prisma.technicalSkill.findMany()
-  const softSkillList = await prisma.technicalSkill.findMany()
-  const projectList = await prisma.technicalSkill.findMany()
+  const softSkillList = await prisma.softSkill.findMany()
+  const projectList = await prisma.project.findMany()
 
   return NextResponse.json({
     technicalSkillList,
